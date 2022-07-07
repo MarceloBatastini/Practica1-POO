87238a4c4262ab8949d97b5c2efa49e3b5211b77
MarceloBatastini <marcelo-bats@hotmail.com>
2022-07-07 07:06:10 -0300

    primera subida

---
+++ Practica/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Telefono telefono1 = new Telefono("Novel", "LG");
+            telefono1.NumeroTelefonico = "1520334265";
+            telefono1.CodigoOperador = 1;
+
+            Telefono telefono2 = new Telefono("K9", "Samsung");
+            telefono2.CodigoOperador = 3;
+            telefono2.NumeroTelefonico = "1532205945";
+
+            Telefono telefono3 = new Telefono("A3", "Motorola");
+            telefono3.CodigoOperador = 5;
+            telefono3.NumeroTelefonico = "034662288";
+
+            Console.WriteLine("El modelo es " + telefono1.Modelo + " y la marca es: " + telefono1.Marca);
+            Console.WriteLine("El numero de telefono es: " + telefono1.NumeroTelefonico + " y su codigo de area es: "+ telefono1.CodigoOperador);
+            
+
+            Console.WriteLine("El numero de telefono es: " + telefono2.NumeroTelefonico + " y su codigo de area es: " + telefono2.CodigoOperador);
+            Console.WriteLine("El numero de telefono es: " + telefono3.NumeroTelefonico + " y su codigo de area es: " + telefono3.CodigoOperador);
+
+            Console.WriteLine(telefono1.Llamar());
+
+
+            Console.WriteLine(telefono3.Llamar("Juan Jose Lopez"));
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Practica/Telefono.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    internal class Telefono
+    {
+        
+        //private string modelo;
+        //private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+
+        public string Modelo { get;}
+        public string Marca { get;}
+        public string NumeroTelefonico { get; set; }
+
+        public int CodigoOperador
+        {
+            get
+            { return codigoOperador; }
+            set
+            {
+                if (value > 0 && value < 4)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+                    }
+        }
+
+        public Telefono (string Modelo, string Marca) 
+        { 
+            this.Modelo = Modelo; 
+            this.Marca = Marca; 
+        }
+
+       
+        public string Llamar ()
+           {
+               return "Realizando llamada .....";
+           }
+
+        
+        public string Llamar (string contacto) 
+        {
+            return "Llamando a " + contacto + numeroTelefonico;
+        }
+
+    }
+
+}
